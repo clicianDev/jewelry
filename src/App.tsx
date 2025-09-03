4132859cec0e3e9a93556677c1a02418136ca157
clicianDev <ceedy12345@gmail.com>
2025-09-03 21:34:46 +0800

    Refactor imports and adjust OrbitControls properties in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Environment, Stage } from "@react-three/drei";
+import { OrbitControls, Environment } from "@react-three/drei";
 import Ring from "./components/Ring";
 
 export default function App() {
@@ -8,11 +8,11 @@ export default function App() {
       <Canvas camera={{ position: [50, 50, 50], fov: 50 }}>
         {/* Global lighting */}
         <ambientLight intensity={0.3} />
-        <hemisphereLight args={[0xffffff, 0x222222, 0.4]} />
-        <directionalLight position={[5, 5, 5]} intensity={1.2} />
-        <Environment preset="city" />
+           <hemisphereLight args={[0xffffff, 0x222222, 0.4]} />
+          <directionalLight position={[5, 5, 5]} intensity={1.2} />
+          <Environment preset="city" />
         <Ring />
-        <OrbitControls />
+        <OrbitControls maxDistance={200} minDistance={50}/>
       </Canvas>
     </div>   
   );         
