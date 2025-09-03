3f7134938452a5d68a2ecfb7bd3848710e575f3a
clicianDev <ceedy12345@gmail.com>
2025-09-03 09:30:26 +0800

    Update camera position in Canvas and import Stage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Environment } from "@react-three/drei";
+import { OrbitControls, Environment, Stage } from "@react-three/drei";
 import Ring from "./components/Ring";
 
 export default function App() {
   return (
     <div style={{ height: "100vh" }}>
-      <Canvas camera={{ position: [5, 5, 5], fov: 50 }}>
+      <Canvas camera={{ position: [50, 50, 50], fov: 50 }}>
         {/* Global lighting */}
         <ambientLight intensity={0.3} />
         <hemisphereLight args={[0xffffff, 0x222222, 0.4]} />
